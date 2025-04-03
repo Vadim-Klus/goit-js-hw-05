e4235aeb40106f329a34f505d0cafd3cf993c657
Vadim Klus <vadim178963@gmail.com>
2025-04-04 00:45:51 +0300

    task1

--- js/task-3.js
+++ js/task-3.js
@@ -1,30 +1,42 @@
-function createProfile(username, playTime) {
 
-    return {
-        username,
-        playTime,
+
+
+
+
+
+
+
+
+
+
+
+// function createProfile(username, playTime) {
+
+//     return {
+//         username,
+//         playTime,
         
-        getInfo() {
-            return `${this.username} has ${this.playTime} active hours!`;
-        },
+//         getInfo() {
+//             return `${this.username} has ${this.playTime} active hours!`;
+//         },
 
-        changeUsername(newName) {
-            this.username = newName;
-        },
+//         changeUsername(newName) {
+//             this.username = newName;
+//         },
 
-        updatePlayTime(hours) {
-            this.playTime += hours;
-        }
+//         updatePlayTime(hours) {
+//             this.playTime += hours;
+//         }
         
-    };
-}
+//     };
+// }
   
-   const profile = createProfile("Jacob", 300);
+//    const profile = createProfile("Jacob", 300);
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+// profile.changeUsername("Marco");
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
